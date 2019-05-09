30bbfd7b3965818b12fb5e54a7b84043fd9651d3
iyinraphael <iyinraphael@gmail.com>
2019-05-09 15:47:08 -0700

    created queue with it's dequeue, enqueue, peek etc methods

---
+++ Algorithms and Data Structures.playground/Pages/Queue.xcplaygroundpage/Contents.swift
@@ -0,0 +1,82 @@
+
+import Foundation
+
+/** MARK :- Queue is a FIFO data Structure and implements the following seven operations
+ 
+ - enqueue() - Adds an element to the back of the queue
+ - dequeue() - Removes and returns the first element from the queue
+ - peek()  - Returns first element of the queue but doesn't remove it
+ - clear() - Resets the queue to an empty state
+ - count - Returns the number of elements in the queue
+ - isEmpty() - Returns true if the queue is empty and false otherwise
+ - isFull() - Returns try if the queue is full and flase otherwise
+ 
+ Implemetation are:
+ 
+ - capacity - A read/write prperty for retrieving or setting the queue capacity
+ - insert(_ :atIndex) - A method that inserts an element at an index specified
+ Tips
+ 
+ Array.capacity returns how many elements it can hold, while Array.count returns how many elements in the array
+ 
+ **/
+
+public struct Queue<T> {
+    private var data = Array<T>()
+    
+    public init() {}
+    
+    public mutating func deque() -> T? {
+        return data.removeFirst()
+    }
+    
+    public func peek() -> T? {
+        return data.first
+    }
+    
+    //MARK:- Helpers for a Circular Buffer
+    public mutating func clear() {
+        data.removeAll()
+    }
+    
+    public mutating func enqueue(_ element: T) {
+        data.append(element)
+    }
+    
+    public var count: Int {
+        return data.count
+    }
+    
+    public var capacity: Int {
+        get {
+            return data.capacity
+        }
+        set {
+            data.reserveCapacity(newValue)
+        }
+    }
+    
+    public func isFull() -> Bool {
+        return count == data.capacity
+    }
+    
+    public func isEmpty() -> Bool {
+        return data.isEmpty
+    }
+    
+}
+
+var queue = Queue<Int>()
+
+queue.enqueue(100)
+queue.enqueue(120)
+queue.enqueue(125)
+queue.enqueue(130)
+
+let x = queue.deque()
+let y = queue.peek()
+let z = queue.deque()
+
+
+
+
